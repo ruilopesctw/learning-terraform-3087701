0ca8ab2ffb0091d36e0e2fc467019d3cf80a43f7
ruilopesctw <138870004+ruilopesctw@users.noreply.github.com>
2023-08-10 11:08:26 +0100

    Correction on text

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id = module.vpc.public_subnets[0]
 
