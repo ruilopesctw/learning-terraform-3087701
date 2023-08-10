872ab612b35dc6f426d1247246911fba0635132c
ruilopesctw <138870004+ruilopesctw@users.noreply.github.com>
2023-08-10 16:15:37 +0100

    Major changes

--- main.tf
+++ main.tf
@@ -85,20 +85,12 @@ module "blog_alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
+
   name    = "blog"
-
-  vpc_id = module.blog_vpc.public_subnets[0]
-
+  vpc_id = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
 }
