566abfb07dca3631884e31edb105110d1e3d7395
ruilopesctw <138870004+ruilopesctw@users.noreply.github.com>
2023-08-10 14:40:53 +0100

    More blog vpc fixes

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -100,7 +100,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  blog_vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
